4f50030da4dff78e8864628cf7182a77cc03f525
pgoggles <praneeth.gogineni@gmail.com>
2021-03-10 14:48:02 -0700

    Implemented fixes on functional to make more efficient

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,25 +1,15 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
   someInstance.push = function(value) {
-    var size = 0;
-    for (var key in storage) {
-      size ++;
-    }
-    storage[size + 1] = value;
+    var size = someInstance.size();
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
-    var size = 0;
-    for (var key in storage) {
-      size ++;
-    }
-    poppedValue = storage[size];
-    delete storage[size];
+    var size = someInstance.size();
+    poppedValue = storage[size - 1];
+    delete storage[size - 1];
     return poppedValue;
   };
 
