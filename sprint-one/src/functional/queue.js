0155ae361d9706c7e08f8549636d996d0d627725
pgoggles <praneeth.gogineni@gmail.com>
2021-03-10 13:34:43 -0700

    Completed Queue and Stack for both functional and functional-shared

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,33 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var size = 0;
+    for (var key in storage) {
+      size ++;
+    }
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    var size = 0;
+    for (var key in storage) {
+      size ++;
+    }
+    dequeuedValue = storage[0];
+    for (var i = 1; i < size; i++) {
+      storage[i - 1] = storage[i];
+    }
+    delete storage[size - 1];
+    return dequeuedValue;
+
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      size ++;
+    }
+    return size;
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,30 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var size = 0;
+    for (var key in storage) {
+      size ++;
+    }
+    storage[size + 1] = value;
   };
 
   someInstance.pop = function() {
+    var size = 0;
+    for (var key in storage) {
+      size ++;
+    }
+    poppedValue = storage[size];
+    delete storage[size];
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      size ++;
+    }
+    return size;
   };
-
   return someInstance;
 };
+
